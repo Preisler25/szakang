c6e26c23366cb5648c35fd3f64f1c164f21eea3c
Preisler <preisler.andras@gmail.com>
2024-02-10 11:57:27 +0100

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+# you can comment with a hash
+# instade of curly braces, python uses colons for function and loop blocks
+def main():
+    # you don't need to declare variable types
+    b = 2
+    # you don't need to use curly braces for loops
+    for a in range(5):
+        # you don't need to use parentheses for if statements
+        if a < b:
+            # you don't need to use curly braces for code blocks
+            # you don't need to use semicolons to end lines
+            print("a is less than b")
+        else:
+            print("a is greater than or equal to b")
+main()
